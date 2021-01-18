b0161bca08a3cb81c5690b0790186464fe0ebb68
Koshikawa Yasunori <y-koshikawa@skyhobbit.co.jp>
2021-01-18 23:42:07 +0900

    閾値の変更、内部処理の変更

--- getCatalyst2960Temperature.sh
+++ getCatalyst2960Temperature.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
-THRESHOLD=45
+THRESHOLD=50
 IPADDRESSES=("192.168.0.52" "192.168.0.53")
 CID=public
 
 date
-MSG=''
 
 for IPADDRESS in ${IPADDRESSES[@]}
 do
   TEMPERATURE=`snmpget -v 2c -c ${CID} ${IPADDRESS} .1.3.6.1.4.1.9.9.13.1.3.1.3.1008 | awk 'END{print $NF}'`
-  echo "$IPADDRESS $TEMPERATURE"
-  if [ $TEMPERATURE -ge $THRESHOLD ] ; then
-    MSG="${MSG} ${TEMPERATURE} "
+  if [[ "${TEMPERATURE}" =~ ^[0-9]+$ ]] ; then
+    echo "$IPADDRESS $TEMPERATURE"
+    if [ $TEMPERATURE -ge $THRESHOLD ] ; then
+      say "${TEMPERATURE}"
+    fi
   fi
 done
-
-say ${MSG}
